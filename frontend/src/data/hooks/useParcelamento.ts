39a77bf1b428876a6b6a3e4a1d9b589c848928de
guxkswzz <163546889+guxkswzz@users.noreply.github.com>
2024-11-29 22:28:46 -0300

    Delete frontend directory

--- frontend/src/data/hooks/useParcelamento.ts
+++
@@ -1,5 +0,0 @@
-import { CalcularParcelamento, QTDE_MAX_PARCELAS } from '@gstore/core'
-
-export default function useParcelamento(valor: number, quantidade: number = QTDE_MAX_PARCELAS) {
-    return new CalcularParcelamento().executar(valor, quantidade)
-}
