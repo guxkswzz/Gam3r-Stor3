c8d69df22c34459ebb2a55fdaec590a1360c09f0
guxkswzz <163546889+guxkswzz@users.noreply.github.com>
2024-11-29 22:28:34 -0300

    Delete apps/backend directory

--- apps/backend/prisma/migrations/20241001132035_usuarios/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "usuarios" (
-    "id" SERIAL NOT NULL,
-    "nome" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "senha" TEXT NOT NULL,
-
-    CONSTRAINT "usuarios_pkey" PRIMARY KEY ("id")
-);
